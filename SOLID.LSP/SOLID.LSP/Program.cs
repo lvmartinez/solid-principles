a99ded3ed2b43ad7945b8a58737c52bb295b9668
lvmartinez <lucia.martinez@acklenavenue.com>
2021-10-19 06:15:12 -0600

    Add LSP example

---
+++ SOLID.LSP/SOLID.LSP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SOLID.LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
+            Calculator sum = new SumCalculator(numbers);
+            Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+            Console.WriteLine();
+            Calculator evenSum = new EvenNumbersSumCalculator(numbers);
+            Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}");
+        }
+    }
+}
